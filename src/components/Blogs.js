51f546ec888afcc1da6a0e30d8b300eb69dfb91d
jayrboy <j.jayox_@hotmail.com>
2023-12-30 23:00:55 +0700

    Add Search Blogs.js Blogs.css
    Add Search Blogs.js Blogs.css

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
 import blogs from "../data/blogs";
 import "./Blogs.css";
+import { useState } from "react";
 
 export default function Blog() {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="blogs-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="ค้นหาบทความ"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <article>
         {blogs.map((item) => (
           <Link to={`/blog/${item.id}`} key={item.id}>

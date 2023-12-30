520883a1d0c4e1cda0cde0dd9eab458a70982718
jayrboy <j.jayox_@hotmail.com>
2023-12-30 22:37:29 +0700

    Create State Detail.js
    Create State Detail.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,21 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import blogs from "../data/blogs";
 
 export default function Details() {
   const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
 
   useEffect(() => {
     // ดึงข้อมูลบทความ
     const result = blogs.find((item) => item.id === parseInt(id));
-    console.log(result);
+    setTitle(result.title);
+    setImage(result.image_url);
+    setContent(result.content);
+    setAuthor(result.author);
   }, [id]);
 
   return (

851af022e0e700eb16f363b7e1c1d9fcfb8b0f1c
jayrboy <j.jayox_@hotmail.com>
2023-12-30 22:28:05 +0700

    Display Console useEffect
    Display Console useEffect

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,15 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import blogs from "../data/blogs";
 
 export default function Details() {
   const { id } = useParams();
+
+  useEffect(() => {
+    // ดึงข้อมูลบทความ
+    const result = blogs.find((item) => item.id === parseInt(id));
+    console.log(result);
+  }, [id]);
 
   return (
     <>

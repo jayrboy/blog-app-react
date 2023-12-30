02d67cdb567dbef058fc5e6c0ea6f1c69c7a6c04
jayrboy <j.jayox_@hotmail.com>
2023-12-30 17:51:46 +0700

    Add Error 404
    Add Error 404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Blogs from "./components/Blogs";
 import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h3>ไม่พบหน้าเว็บ (404 Page Not Found)</h3>;
+}
